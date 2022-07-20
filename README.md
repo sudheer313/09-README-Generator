d835575c92ee2a28a2f25a4c1ed5158df35141db
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-07-20 10:48:48 +1000

    updated js

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Professional README Generator Starter Code
+# read me
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
--- index.js
+++ index.js
@@ -2,15 +2,78 @@
 const inquirer=require('inquirer');
 const fs = require('fs');
 const util =require('util');
-const generatorMarkdown= require('\Develop\utils\generateMarkdown.js');
+const generateReadMe= require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project ?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide description of your project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:"What are the Installation steps for your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide usage of your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose the appropriate license?",
+    choices: ["Apache 2.0", "Boost", "MIT", "Mozilla", "IBM","none"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Would you like other developers to contribute?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Did you write any test for your application?",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Please provide your gitHub username ?",
+    
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please provide your email address?",
+  },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateReadMe(data), (err) =>{
+        if (err)
+        console.log(err);
+        else{
+            console.log("sucess! Readme is generated")
+        }
+    })
+
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+console.log("welcome to Node !! Generate a Read me");
+inquirer.prompt(questions).then((response)=>{
+    writeToFile('./README.md', response)
+    console.log(response);
 
+});
+}
 // Function call to initialize app
 init();
